a5344bd7fdb38af67a6eab59e806274683968726
julianort11 <ort.na140@gmail.com>
2025-05-06 20:37:50 -0500

    feat: :sparkles: continue with the page home

--- scr/js/main.js
+++ scr/js/main.js
@@ -1,15 +1,18 @@
-const palabras = ["Julian Ortega", "Wed Developer", "full-stack in process", "Tech Nerd "];
-  const span = document.getElementById("changing-word");
-  let index = 0;
-
-  function cambiarPalabra() {
-    span.style.opacity = 0;
-    setTimeout(() => {
-      span.textContent = palabras[index];
-      span.style.opacity = 1;
-      index = (index + 1) % palabras.length;
-    }, 500);
-  }
-
-  cambiarPalabra();
-  setInterval(cambiarPalabra, 3000);
+document.addEventListener("DOMContentLoaded", () => {
+    new TypeIt("#changing-word", {
+      strings: ["Julian Ortega", "Web Developer", "Full-Stack in process", "Tech Nerd"],
+      speed: 100,
+      breakLines: false,
+      loop: true,
+      deleteSpeed: 50,
+      waitUntilVisible: true,
+    }).go();
+  
+    // Parallax olas
+    const escenaOlas = document.getElementById('olas');
+    if (escenaOlas) {
+      new Parallax(escenaOlas);
+    }
+  });
+  
+  
